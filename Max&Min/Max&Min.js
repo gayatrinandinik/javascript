98c93ff03f19e6236a77e091bb9537eb679ac87d
gayatri <gayatrinandini.k@gmail.com>
2026-06-29 16:50:11 +0530

    Max&Min

---
+++ Max&Min/Max&Min.js
@@ -0,0 +1,17 @@
+function onClickMax(){
+    debugger;
+    let a=document.getElementById("txtNum1").value;
+    let b=document.getElementById("txtNum2").value;
+    let c=document.getElementById("txtNum3").value;
+    let max=Math.max(a,b,c);
+    alert(max);
+}
+
+function onClickMin(){
+    debugger;
+     let a=document.getElementById("txtNum1").value;
+    let b=document.getElementById("txtNum2").value;
+    let c=document.getElementById("txtNum3").value;
+    let min=Math.min(a,b,c);
+    alert(min);
+}
