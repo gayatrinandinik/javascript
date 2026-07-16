249344f63ad2c01c18546c53bd846be6775ed85b
gayatri <gayatrinandini.k@gmail.com>
2026-07-16 15:44:39 +0530

    feat(EmiCalculation).add Emi function

---
+++ EmiCalculation/EmiCalculation.js
@@ -0,0 +1,6 @@
+function onClickCheck(){
+    debugger;
+    let a = document.getElementById("txtNum").value;
+    let b = document.getElementById("txtMonth").value;
+    alert((a/b).toFixed(3));
+}
