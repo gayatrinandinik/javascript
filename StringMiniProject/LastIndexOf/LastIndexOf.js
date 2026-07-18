66286ac94a58bc1790dcee6c46973e1975d5d6bc
gayatri <gayatrinandini.k@gmail.com>
2026-07-18 19:04:59 +0530

    feat(LastIndexOf).add last index of

---
+++ StringMiniProject/LastIndexOf/LastIndexOf.js
@@ -0,0 +1,16 @@
+numbers=["1","2","3","4","5","6","7"]
+function onClickPush(){
+    debugger;
+    var number=document.getElementById("txtNum").value;
+    document.getElementById("txtNum").value="";
+    numbers.push(number);
+    document.getElementById("pResult").innerHTML=numbers;
+
+}
+
+function onClickCheck(){
+    debugger;
+    var search=document.getElementById("txtSearch").value;
+    document.getElementById("txtSearch").value="";
+    document.getElementById("pLastIndexOfResult").innerHTML = numbers.lastIndexOf(search);
+}
